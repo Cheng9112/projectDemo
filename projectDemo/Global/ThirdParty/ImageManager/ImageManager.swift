6dac2d9680925bd45ec3818e44346ff3da4301f7
cheng <cheng9112@icloud.com>
2022-06-28 22:46:23 +0800

    补充

--- projectDemo/Global/ThirdParty/ImageManager/ImageManager.swift
+++ projectDemo/Global/ThirdParty/ImageManager/ImageManager.swift
@@ -8,14 +8,20 @@
 import Foundation
 import Kingfisher
 
+/// 图片下载与缓存
+class ImageDCManager {
+    
+}
+
+/// UIImageView 图片缓存扩展
 extension UIImageView {
     
     public func setNetImage(imageUrlStr: String) {
         
-        self.setNetImage(imageUrlStr: imageUrlStr, placeholder: UIImage.init())
+        self.setNetImage(imageUrlStr: imageUrlStr, placeholder: nil)
     }
     
-    public func setNetImage(imageUrlStr: String, placeholder: UIImage) {
+    public func setNetImage(imageUrlStr: String, placeholder: UIImage?) {
         
         if let url = URL(string: imageUrlStr) {
             
@@ -32,8 +38,33 @@ extension UIImageView {
                 }
             }
         } else {
-            
             LogInfo("图片链接为空")
+        }
+    }
+}
+
+/// UIButton 图片缓存扩展
+extension UIButton {
+    
+    public func setNetImage(imageUrlStr: String, state: UIControl.State) {
+        
+        self.setNetImage(imageUrlStr: imageUrlStr, state: state, placeHolder: nil)
+    }
+    
+    public func setNetImage(imageUrlStr: String, state: UIControl.State, placeHolder: UIImage?) {
+        
+        if let url = URL(string: imageUrlStr) {
+            let processor = DownsamplingImageProcessor(size: bounds.size) |> RoundCornerImageProcessor(cornerRadius: 20)
+            kf.setImage(with: url, for: state, placeholder: placeHolder, options: [.processor(processor),.scaleFactor(UIScreen.main.scale),.transition(.fade(1)),.cacheOriginalImage], progressBlock: nil) { result in
+                switch result {
+                case .success(let value):
+                    LogInfo("Task done for: \(value.source.url?.absoluteString ?? "")")
+                case .failure(let error):
+                    LogInfo("Job failed: \(error.localizedDescription)")
+                }
+            }
+        } else {
+           LogInfo("图片链接为空")
         }
     }
 }
